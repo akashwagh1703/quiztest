b3387bd9f600a0f3a826fa9ab641b1bea6e7c643
Akash Wagh <Akash.Wagh@esds.co.in>
2025-02-03 12:54:44 +0530

    Get master update constant v1

--- src/config/Constant.js
+++ src/config/Constant.js
@@ -1,4 +1,4 @@
 const isLocal = window.location.hostname === "localhost";
 
-export const BASEURL = isLocal ? "http://localhost" : "https://quiztest-gilt.vercel.app";
-export const APIURL = isLocal ? "http://localhost:5000/" : "https://quiztest-gilt.vercel.app/api/";
+export const BASEURL = isLocal ? "http://localhost" : "https://quiztest-roan.vercel.app";
+export const APIURL = isLocal ? "http://localhost:5000/" : "https://quiztest-roan.vercel.app/api/";
